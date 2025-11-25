e0cb800bcea5b3951064eca0c4f83939fc985d79
FaridMo99 <farid-ahmad@hotmail.de>
2025-11-25 10:26:02 +0100

    finished user favorites route

---
+++ frontend/app/(protected)/user/favorites/components/ProductGrid.tsx
@@ -0,0 +1,15 @@
+import ProductCard from '@/components/main/ProductCard'
+import { Product } from '@/types/types'
+
+function ProductGrid({products}:{products:Product[]}) {
+  return (
+    <div
+      className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductGrid
--- frontend/app/(protected)/user/favorites/page.tsx
+++ frontend/app/(protected)/user/favorites/page.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client"
+import LoadingPage from "@/components/main/LoadingPage";
+import { getUserFavoriteItems } from "@/lib/queries/client/usersQueries";
+import useAuth from "@/stores/authStore";
+import { useQuery } from "@tanstack/react-query";
+import ProductGrid from "./components/ProductGrid";
 
-function page() {
-  return <div>page</div>;
+//error page
+function Page() {
+  const accessToken = useAuth(state => state.accessToken)
+  
+  const { data: favoriteProducts, isLoading, isError, error } = useQuery({
+    queryKey: ["get user favorite products"],
+    queryFn:()=>getUserFavoriteItems(accessToken!)
+  })
+
+  if (isLoading) return <LoadingPage />
+  if (isError) throw error
+  
+  return (
+    <main>
+      <h2 className="text-3xl font-extrabold mb-2 pl-8">
+        Favorites({favoriteProducts?.length})
+      </h2>
+      <div className="w-full flex flex-col justify-between items-center">
+        <ProductGrid products={favoriteProducts ?? []}/>
+      </div>
+    </main>
+  );
 }
 
-export default page;
+export default Page;
