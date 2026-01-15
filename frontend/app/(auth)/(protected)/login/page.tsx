be46e911153413e9ca32be50b1fd0767fa9d662f
FaridMo99 <farid-ahmad@hotmail.de>
2026-01-15 17:40:03 +0100

    added auto fill for form

--- frontend/app/(auth)/(protected)/login/page.tsx
+++ frontend/app/(auth)/(protected)/login/page.tsx
@@ -1,20 +1,10 @@
 import { LoginForm } from "@/components/forms/login-form";
-import { Card, CardContent } from "@/components/ui/card";
-import { STRIPE_ENV } from "@/config/constants";
 
 async function page(props: PageProps<"/login">) {
   const { error } = await props.searchParams;
 
   return (
-    <main>
       <LoginForm error={!!error} />
-      {STRIPE_ENV === "testing" && (
-        <Card className="bg-backgroundBright text-white mt-4">
-          <CardContent>Testing Email-Address: Test@gmail.com</CardContent>
-          <CardContent>Testing Password: Test123</CardContent>
-        </Card>
-      )}
-    </main>
   );
 }
 
