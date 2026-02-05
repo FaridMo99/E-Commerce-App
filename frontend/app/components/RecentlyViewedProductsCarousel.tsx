4909b4543e9c037e647128ac9e4fbe257ba2588d
FaridMo99 <farid-ahmad@hotmail.de>
2026-02-05 04:33:03 +0100

    fixed carousel responsiveness

--- frontend/app/components/RecentlyViewedProductsCarousel.tsx
+++ frontend/app/components/RecentlyViewedProductsCarousel.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Slider, { type Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { getRecentlyViewedProducts } from "@/lib/queries/client/usersQueries";
@@ -7,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "@/stores/authStore";
 import ProductCard from "@/components/main/product/ProductCard";
 import SectionWrapper from "@/components/main/SectionWrapper";
+import BaseSlider from "@/components/main/BaseSlider";
 
 function RecentlyViewedProductsCarousel() {
   const accessToken = useAuth((state) => state.accessToken);
@@ -23,30 +23,15 @@ function RecentlyViewedProductsCarousel() {
     enabled: !!accessToken,
   });
 
-  const settings: Settings = {
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    swipe: true,
-    swipeToSlide: true,
-    arrows: true,
-    infinite: true,
-
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 4 } },
-      { breakpoint: 820, settings: { slidesToShow: 3 } },
-      { breakpoint: 640, settings: { slidesToShow: 2 } },
-    ],
-  };
-
   if (!products || products.length === 0 || isError || isLoading) return null;
 
   return (
     <SectionWrapper styles="" header="Recently Viewed">
-      <Slider {...settings}>
+      <BaseSlider>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
-      </Slider>
+      </BaseSlider>
     </SectionWrapper>
   );
 }
--- frontend/components/main/BaseSlider.tsx
+++ frontend/components/main/BaseSlider.tsx
@@ -13,9 +13,10 @@ export const settings: Settings = {
   infinite: true,
 
   responsive: [
-    { breakpoint: 1024, settings: { slidesToShow: 4 } },
-    { breakpoint: 820, settings: { slidesToShow: 3 } },
-    { breakpoint: 640, settings: { slidesToShow: 2 } },
+    { breakpoint: 1270, settings: { slidesToShow: 4 } },
+    { breakpoint: 1020, settings: { slidesToShow: 3 } },
+    { breakpoint: 790, settings: { slidesToShow: 2 } },
+    { breakpoint: 540, settings: { slidesToShow: 1 } },
   ],
 };
 
