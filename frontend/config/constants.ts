145e48e0cc1057a70e15fea3e3189c70c7ee259f
FaridMo99 <farid-ahmad@hotmail.de>
2026-04-02 13:29:36 +0200

    added nginx

--- frontend/config/constants.ts
+++ frontend/config/constants.ts
@@ -1,6 +1,5 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL!;
-export const DOCKER_BASE_URL =
-  process.env.NEXT_PUBLIC_BACKEND_DOCKER_INTERNAL_URL!;
+export const DOCKER_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_DOCKER_INTERNAL_URL!;
 export const API_BASE_URL = BASE_URL + "/api";
 export const API_DOCKER_BASE_URL = DOCKER_BASE_URL + "/api";
 export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!;
@@ -8,7 +7,7 @@ export const DOMAIN_NAME = process.env.NEXT_PUBLIC_DOMAIN_NAME!;
 export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN!;
 export const IS_DOCKER_CONTAINER = process.env.NEXT_PUBLIC_DOCKER_ENV!;
 
-export const getApiUrl = () => {
+export function getApiUrl():string {
   if (typeof window !== "undefined") {
     return API_BASE_URL
   }
